8319246b995a642efded8e80aff3c15d07174d9e
TarunkumarN <99187044+TarunkumarN@users.noreply.github.com>
2022-02-08 10:35:22 +0530

    Add files via upload

---
+++ Controllers/CustController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_on_Customer_Single_view_EF.Models;
+
+namespace CRUD_on_Customer_Single_view_EF.Controllers
+{
+    public class CustController : Controller
+    {
+        public void ForewardCust()
+        {
+            CustomerModel obj = new CustomerModel();
+            ViewBag.cust = obj.GetCustomer();
+        }
+        // GET: Cust
+        public ActionResult Index()
+        {
+            ForewardCust();
+            return View();
+        }
+        public ActionResult DeleteCus()
+        {
+            CustomerModel obj = new CustomerModel
+            {
+                Cid = int.Parse(Request.QueryString["cno"])
+            };
+            ViewData["delvalue"] = obj.DeleteCustomer();
+            ForewardCust();
+            return View("Index");
+        }
+        public ActionResult InsertCus()
+        {
+            CustomerModel obj = new CustomerModel
+            {
+                Cid = int.Parse(Request["txtcno"]),
+                cname = Request["txtcname"],
+                cloc = Request["txtcloc"],
+                cmail = Request["txtcmail"]
+            };
+            ViewData["insvalue"] = obj.InsertCustomer();
+            ForewardCust();
+            return View("Index");
+        }
+        public ActionResult EditCus()
+        {
+            CustomerModel obj = new CustomerModel
+            {
+                Cid = int.Parse(Request.QueryString["cno"])
+            };
+            ForewardCust();
+            return View("Index");
+        }
+        public ActionResult UpdateCus()
+        {
+            CustomerModel obj = new CustomerModel
+            {
+                Cid = int.Parse(Request["txtcid"]),
+                cloc = Request["txtnewloc"]
+            };
+            ViewData["updvalue"] = obj.UpdateCustomer();
+            ForewardCust();
+            return View("Index");
+        }
+        public ActionResult Cancel()
+        {
+            ForewardCust();
+            return View("Index");
+        }
+    }
+}
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_on_Customer_Single_view_EF.Models
+{
+    public class CustomerModel
+    {
+        public int Cid { get; set; }
+        public string cname { get; set; }
+        public string cloc { get; set; }
+        public string cmail { get; set; }
+        collegeEntities1 objef = new collegeEntities1();
+        public IQueryable GetCustomer()
+        {
+            return objef.customers;
+        }
+        public int InsertCustomer()
+        {
+            customer objcus = new customer();
+            objcus.custid = Cid;
+            objcus.custname = cname;
+            objcus.custloc = cloc;
+            objcus.cmailid = cmail;
+            try
+            {
+                objef.customers.Add(objcus);
+                objef.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        public int DeleteCustomer()
+        {
+            customer objcus = objef.customers.Find(Cid);
+            try
+            {
+                objef.customers.Remove(objcus);
+                objef.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        public int UpdateCustomer()
+        {
+            customer objcus = objef.customers.Find(Cid);
+            objcus.custloc = cloc;
+            try
+            {
+                objef.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
